0eb95b7898b92832d79e87ace69d8e338b746a4a
Alexei Khlebnikov <alexei.khlebnikov@gmail.com>
2020-08-30 22:14:19 +0200

    Added simple API for random number generation.

---
+++ src/main/kotlin/no/bspoke/kotlinrandom/RestController.kt
@@ -0,0 +1,16 @@
+package no.bspoke.kotlinrandom
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class RestController {
+    @GetMapping("/random-number")
+    fun getRandomNumber(@RequestParam(defaultValue = "6") max: Int): String {
+        val randomNumber = (1..max).random()
+        return "{ \"randomNumber\": $randomNumber }"
+    }
+}
