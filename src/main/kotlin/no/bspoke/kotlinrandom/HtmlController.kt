60151a768d5157ec63cb183f1cbff547576eec26
Alexei Khlebnikov <alexei.khlebnikov@gmail.com>
2020-08-30 21:11:46 +0200

    Added homepage.

---
+++ src/main/kotlin/no/bspoke/kotlinrandom/HtmlController.kt
@@ -0,0 +1,16 @@
+package no.bspoke.kotlinrandom
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+    @GetMapping("/")
+    fun homepage(model: Model): String {
+        model["title"] = "Home Page"
+        return "homepage"
+    }
+}
